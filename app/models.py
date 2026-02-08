491b9fd508ea5acc3ad54da26b553d827ab62501
aeshikaz <aeshikasingh@gmail.com>
2026-02-08 19:56:59 +0530

    fixed merge problem

--- app/models.py
+++ app/models.py
@@ -1,39 +1,5 @@
 from .extensions import db
 from flask_login import UserMixin
-
-# USER MODEL (for login)
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(200))
-
-
-# EXPENSE MODEL
-class Expense(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200))
-    amount = db.Column(db.Float)
-    paid_by = db.Column(db.String(100))
-    group_id = db.Column(db.Integer)
-
-
-# EXPENSE SPLIT
-class ExpenseSplit(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    expense_id = db.Column(db.Integer)
-    user_id = db.Column(db.String(100))
-    share_amount = db.Column(db.Float)
-
-
-# LOCATION MODEL
-class UserLocation(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    group_id = db.Column(db.Integer)
-    latitude = db.Column(db.Float)
-    longitude = db.Column(db.Float)
-
 from datetime import datetime
 
 # ---------- USER ----------
@@ -43,6 +9,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
 
+
 # ---------- GROUPS ----------
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -50,7 +17,35 @@ class Group(db.Model):
     invite_code = db.Column(db.String(10), unique=True, nullable=False)
     created_by = db.Column(db.Integer, db.ForeignKey("user.id"))
 
+
 class GroupMember(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     group_id = db.Column(db.Integer, db.ForeignKey("group.id"))
+
+
+# ---------- EXPENSE ----------
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    amount = db.Column(db.Float)
+    paid_by = db.Column(db.String(100))
+    group_id = db.Column(db.Integer)
+
+
+# ---------- EXPENSE SPLIT ----------
+class ExpenseSplit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    expense_id = db.Column(db.Integer)
+    user_id = db.Column(db.String(100))
+    share_amount = db.Column(db.Float)
+
+
+# ---------- LOCATION ----------
+class UserLocation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    group_id = db.Column(db.Integer)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+
