045657aba33b2700bd414d93a759becf885eec58
aeshikaz <aeshikasingh@gmail.com>
2026-02-08 16:23:38 +0530

    aeshika added models for expenses and location

---
+++ app/models.py
@@ -0,0 +1,36 @@
+from .extensions import db
+from flask_login import UserMixin
+
+# USER MODEL (for login)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(200))
+
+
+# EXPENSE MODEL
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    amount = db.Column(db.Float)
+    paid_by = db.Column(db.Integer)
+    group_id = db.Column(db.Integer)
+
+
+# EXPENSE SPLIT
+class ExpenseSplit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    expense_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+    share_amount = db.Column(db.Float)
+
+
+# LOCATION MODEL
+class UserLocation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    group_id = db.Column(db.Integer)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+
